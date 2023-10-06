e99ae9fdecc20aefdd1acacba35a1588dfbb02b7
Richard Chen <td231565@gmail.com>
2023-10-06 17:11:18 +0800

    新增Repos組件

---
+++ src/view/repo/components/CardItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+
+const prop = defineProps<{
+  data: any
+}>()
+</script>
+
+<template>
+  <div class="mb-3 px-3 py-1 shadow rounded">
+    <h3 class="mb-1 font-bold text-lg">{{ prop.data.name }}</h3>
+    <p>{{ prop.data.description }}</p>
+    <p class="mt-2">
+      <a
+        :href="prop.data.html_url"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="no-underline ease-in text-green-950 hover:bg-green-400/[0.2]"
+        >{{ prop.data.html_url }}</a
+      >
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/view/repo/index.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import FetchGithubRepos from '@/utils/fetchGithub'
+import { useInfiniteScroll } from '@vueuse/core'
+import CardItem from './components/CardItem.vue'
+import Spinner from './components/LoadingSpinner.vue'
+
+const vueRepos = new FetchGithubRepos(import.meta.env.VITE_GITHUB)
+vueRepos.setOwner('vuejs')
+
+const isLoading = ref(false)
+const isEnd = ref(false)
+const listRef = ref<HTMLElement>()
+const listData = ref<any[]>([])
+
+useInfiniteScroll(
+  listRef,
+  async () => {
+    // load more
+    if (vueRepos.isEnd()) {
+      isEnd.value = true
+      return
+    }
+    isLoading.value = true
+    const nextPage = await vueRepos.getNextPage()
+    listData.value.push(...nextPage)
+    isLoading.value = false
+  },
+  { distance: 10 }
+)
+</script>
+
+<template>
+  <article class="w-[500px] mx-auto pt-8">
+    <h1 class="mb-5 text-lg">
+      Repos from: <span class="text-green-900 font-bold">{{ vueRepos.getOwner() }}</span>
+    </h1>
+    <ul ref="listRef" class="w-full h-[100svh] lg:max-h-[600px] p-3 lg:border overflow-y-auto">
+      <li v-for="repo in listData" :key="repo.id">
+        <CardItem :data="repo" />
+      </li>
+      <li v-if="isLoading" class="text-center p-4">
+        <Spinner />
+      </li>
+      <li v-if="isEnd" class="text-center p-2 text-gray-400">no more</li>
+    </ul>
+  </article>
+</template>
+
+<style scoped></style>
